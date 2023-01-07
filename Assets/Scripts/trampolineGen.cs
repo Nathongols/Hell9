f605502bf4ca88331275e77b77b7b250e9480ab8
black <blackopsrocks11@hotmail.com>
2023-01-07 15:29:47 -0500

    Fixed Trampoline, added slow down mechanic

--- Assets/Scripts/trampolineGen.cs
+++ Assets/Scripts/trampolineGen.cs
@@ -10,27 +10,39 @@ public class trampolineGen : MonoBehaviour
     private Vector2 pos2;
     private float dist;
     private float ang;
+
+    public GameObject Create;
+    public GameObject Create2;
+    public GameObject Create3;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {   
         if (PauseMenu.isPaused == false){
             if (Input.GetKeyDown(KeyCode.Mouse0)){
                 pos1 = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                Time.timeScale = 0.5f;
             } else if (Input.GetKeyUp(KeyCode.Mouse0)){
                 pos2 = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                Time.timeScale = 1f;
                 dist = Vector2.Distance(pos1, pos2);
+                Debug.Log(dist);
                 ang = Mathf.Atan2(pos2.y - pos1.y, pos2.x - pos1.x) * 180 / Mathf.PI;
-                Debug.Log(ang);
                 Quaternion target = Quaternion.Euler(0, 0, ang);
-                Instantiate(myPrefab, pos1, target);
-                myPrefab.transform.localScale = new Vector2(dist, myPrefab.transform.localScale.y);
-
+                GameObject Create = Instantiate(myPrefab, pos1, target);
+                Destroy(Create, 1.5f);
+                float Track = 0;
+                for (float i = 0; i < dist * 10; i++){
+                    GameObject Create2 = Instantiate(myPrefab,Vector3.Lerp(pos1, pos2, Track) , target);
+                    Track += 0.025f;
+                    Destroy(Create2, 1.5f);
+                }
+                GameObject Create3 = Instantiate(myPrefab, pos2, target);
+                Destroy(Create3, 1.5f);
             }
         }
     }
